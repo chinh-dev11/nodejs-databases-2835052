c85a3d7376cfee7d447cc123dacdca46ae709e1d
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2023-12-19 16:46:52 -0500

    Use node.js with Redis

--- maxcoin/main.js
+++ maxcoin/main.js
@@ -1,15 +1,21 @@
 // const CoinAPI = require("./services/CoinAPI");
 const MongoBackend = require('./services/backend/MongoBackend')
+const RedisBackend = require('./services/backend/RedisBackend')
 
-async function run() {
+async function runMongo() {
   // const coinAPI = new CoinAPI();
   // return coinAPI.fetch();
   const mongoBackend = new MongoBackend()
   return mongoBackend.max()
 }
 
-run()
+async function runRedis() {
+  const redisBackend = new RedisBackend()
+  return redisBackend.max()
+}
+
+runRedis()
   .then((result) => {
-    console.log('>>> result:', result)
+    console.log('>>> result:', result) // >>> result: [ '2021-11-08', '67544.8733' ] // the highest price (score)
   })
   .catch((err) => console.error(err))
