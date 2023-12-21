400cd19f75c1629a46f428806166684654f22422
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2023-12-21 13:13:05 -0500

    Implement a basket service

--- shopper/server/routes/shop/index.js
+++ shopper/server/routes/shop/index.js
@@ -1,49 +1,48 @@
-const express = require("express");
+const express = require('express')
 
-module.exports = () => {
-  const router = express.Router();
+const ItemService = require('../../services/ItemService')
+const BasketService = require('../../services/BasketService')
 
-  router.get("/", async (req, res) => {
-    return res.render("shop", {});
-    /*
-    const items = await ItemService.getAll();
-    return res.render('shop', { items });
-    */
-  });
+module.exports = (config) => {
+  const router = express.Router()
 
-  router.get("/tobasket/:itemId", async (req, res, next) => {
-    return next("Not implemented");
+  router.get('/', async (req, res) => {
+    const items = await ItemService.getAll()
+    return res.render('shop', { items })
+  })
 
-    /*
+  router.get('/tobasket/:itemId', async (req, res) => {
     if (!res.locals.currentUser) {
       req.session.messages.push({
-        type: "warning",
-        text: "Please log in first",
-      });
-      return res.redirect("/shop");
+        type: 'warning',
+        text: 'Please log in first',
+      })
+
+      return res.redirect('/shop')
     }
 
     try {
       const basket = new BasketService(
         config.redis.client,
         res.locals.currentUser.id
-      );
-      await basket.add(req.params.itemId);
+      )
+
+      await basket.add(req.params.itemId)
+      
       req.session.messages.push({
-        type: "success",
-        text: "The item was added to the basket",
-      });
+        type: 'success',
+        text: 'The item was added to the basket',
+      })
     } catch (err) {
       req.session.messages.push({
-        type: "danger",
-        text: "There was an error adding the item to the basket",
-      });
-      console.error(err);
+        type: 'danger',
+        text: 'There was an error adding the item to the basket',
+      })
+      console.error(err)
     }
 
-    return res.redirect("/shop");
-    */
-  });
+    return res.redirect('/shop')
+  })
 
-  return router;
-};
+  return router
+}
