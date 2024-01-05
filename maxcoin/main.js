8efc065e63fa7ec2ffe84220a6a5893ad0e431af
Chinh Le <5817525-chinh-dev11@users.noreply.gitlab.com>
2024-01-05 16:07:45 -0500

    Use Node.js with MySQL

--- maxcoin/main.js
+++ maxcoin/main.js
@@ -1,6 +1,7 @@
 // const CoinAPI = require("./services/CoinAPI");
 const MongoBackend = require('./services/backend/MongoBackend')
 const RedisBackend = require('./services/backend/RedisBackend')
+const MySQLBackend = require('./services/backend/MySQLBackend')
 
 async function runMongo() {
   // const coinAPI = new CoinAPI();
@@ -14,8 +15,24 @@ async function runRedis() {
   return redisBackend.max()
 }
 
-runRedis()
+// runRedis()
+//   .then((result) => {
+//     console.log('>>> result:', result) // >>> result: [ '2021-11-08', '67544.8733' ] // the highest price (score)
+//   })
+//   .catch((err) => console.error(err))
+
+async function runMySQL() {
+  const mySQLBackend = new MySQLBackend()
+  return mySQLBackend.max()
+}
+
+runMySQL()
   .then((result) => {
-    console.log('>>> result:', result) // >>> result: [ '2021-11-08', '67544.8733' ] // the highest price (score)
+    console.log('>>> TextRow:', result)
+    /* >>> TextRow: {
+      id: 1035,
+      valuedate: 2021-11-08T05:00:00.000Z,
+      coinvalue: '67633.65'
+    } */
   })
   .catch((err) => console.error(err))
